ab2806f8b055d63832fa725bafdac7603d2a217f
Simone Aronica <40305701+xNicklaj@users.noreply.github.com>
2020-09-21 22:05:51 +0200

    Initial commit

---
+++ development.ps1
@@ -0,0 +1,100 @@
+Write-Host -ForegroundColor Yellow "Chocolatey Disclaimer:
+Your use of the packages on this site means you understand they are not supported or guaranteed in any way. 
+Due to the nature of a public repository and unreliability due to distribution rights, these packages should not be used as is for organizational purposes either."
+Write-Host ""
+#Install chocolatey
+$testchoco = powershell choco -v
+if(-not($testchoco)){
+    Write-Host -ForegroundColor Red "Seems Chocolatey is not installed, installing now"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+else{
+    Write-Host -ForegroundColor Green "Chocolatey Version $testchoco is already installed"
+}
+
+function chocoInstall($arr) {
+	if($mode -eq "m")
+	{
+		Write-Host "Select the packages to install: "
+		for ($i = 0; $i -lt $arr.count; $i++) {
+			Write-Host "$i) $($arr[$i])"
+		}
+	
+		$input = Read-Host -Prompt "Specify packages separated by commas"
+		$input = $input -replace '\s',''
+		$input = $input -Split ','
+	
+		if($input -ne $null)
+		{
+			$input | ForEach-Object {
+				powershell choco install -y $arr[$PSItem]
+			}
+		}
+	}
+	else{
+		$arr | ForEach-Object {
+			powershell choco install -y $PSItem
+		}
+	}
+}
+
+function listPackages($arr) {
+	$arr | ForEach-Object {
+		Write-Host " - $PSItem"
+	}
+}
+
+$editor = @('vscode', 'neovim')
+$webdev = @('nodejs', 'yarn', 'bitnami-xampp', 'nginx')
+$database = @('mariadb', 'mongodb')
+$javadev = @('openjdk', 'openjdk8')
+$desktopdev = @('mingw', 'rust', 'rustup')
+$androiddev = @('androidstudio', 'koktlinc', 'flutter', 'dark-sdk')
+$tool = @('docker-desktop')
+$versioncontrol = @('git', 'github-desktop')
+
+Write-Host "The following packages will be installed: "
+
+Write-Host "--- Editors ---"
+listPackages($editor)
+Write-Host "--- Web ---"
+listPackages($webdev)
+Write-Host "--- Databases ---"
+listPackages($database)
+Write-Host "--- Java ---"
+listPackages($javadev)
+Write-Host "--- Desktop ---"
+listPackages($desktopdev)
+Write-Host "--- Android ---"
+listPackages($androiddev)
+Write-Host "--- Tools ---"
+listPackages($tool)
+Write-Host "--- Version Control ---"
+listPackages($versioncontrol)
+
+do {
+	$mode = Read-Host -Prompt "Select whether you want to perform an [A]utomated or a [m]anual installation"
+	if($mode -eq ""){
+		$mode = "a"
+	}
+} while(($mode.ToLower() -ne "A") -and ($mode.ToLower() -ne "M"))
+
+Write-Host "--- Editors ---"
+chocoInstall($editor)
+Write-Host "--- Web ---"
+chocoInstall($webdev)
+Write-Host "--- Databases ---"
+chocoInstall($database)
+Write-Host "--- Java ---"
+chocoInstall($javadev)
+Write-Host "--- Desktop ---"
+chocoInstall($desktopdev)
+Write-Host "--- Android ---"
+chocoInstall($androiddev)
+Write-Host "--- Tools ---"
+chocoInstall($tool)
+Write-Host "--- Version Control ---"
+chocoInstall($versioncontrol)
+
+Write-Host "Press any key to continue ....."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
---
+++ driver.ps1
@@ -0,0 +1,37 @@
+Write-Host -ForegroundColor Yellow "Chocolatey Disclaimer:
+Your use of the packages on this site means you understand they are not supported or guaranteed in any way. 
+Due to the nature of a public repository and unreliability due to distribution rights, these packages should not be used as is for organizational purposes either."
+Write-Host ""
+#Install chocolatey
+$testchoco = powershell choco -v
+if(-not($testchoco)){
+    Write-Host -ForegroundColor Red "Seems Chocolatey is not installed, installing now"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+else{
+    Write-Host -ForegroundColor Green "Chocolatey Version $testchoco is already installed"
+}
+
+#Nvidia GPU Drivers
+If ((Get-WmiObject Win32_VideoController).name | Select-String -Pattern 'NVIDIA'){
+	Write-Host "Nvidia GPU Detected, installing GPU Drivers..."
+	choco install -y nvidia-display-driver
+	choco install -y geforce-experience
+}
+
+#AMD Ryzen Drivers
+If ((Get-WmiObject Win32_Processor).name | Select-String -Pattern 'AMD Ryzen'){
+	Write-Host "AMD Ryzen CPU Detected, installing CPU Drivers..."
+	choco install -y amd-ryzen-chipset
+}
+
+#Intel Graphics Divers
+If ((Get-WmiObject Win32_Processor).name | Select-String -Pattern 'INTEL'){
+	Write-Host "Intel CPU Detected, installing Intel Graphics Drivers..."
+	choco install -y intel-graphics-driver
+}
+
+Write-Host "Installation finished."
+
+Write-Host "Press any key to continue ....."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
